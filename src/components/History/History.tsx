2fce952d14f4bfe5695c1159ea889e5e7da831e5
nospyrin73 <mohammedzuhair527@gmail.com>
2023-07-09 03:30:08 +0300

    Responsiveness improvements

--- src/components/History/History.tsx
+++ src/components/History/History.tsx
@@ -1,4 +1,4 @@
-import { ReactElement } from 'react';
+import { ReactElement, useState, useLayoutEffect } from 'react';
 import { Carousel } from '../Carousel/Carousel';
 
 import { items } from './dummy_data.json';
@@ -6,6 +6,20 @@ import { items } from './dummy_data.json';
 import './History.scss';
 
 export function History(): ReactElement {
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    useLayoutEffect(() => {
+        const updateWidth = () => setWindowWidth(window.innerWidth);
+
+        window.addEventListener('resize', updateWidth);
+
+        () => window.removeEventListener('resize', updateWidth);
+    }, []);
+
+    const visibleItems = windowWidth < 700 ? 2 : windowWidth < 930 ? 3 : undefined;
+    const itemWidth = windowWidth < 470 ? 170 : undefined;
+
+
     return (
         <div className="history" id="history">
             <article className="history__statement">
@@ -18,7 +32,7 @@ export function History(): ReactElement {
                 </p>
             </article>
             <section className="history__carousel-wrapper">
-                <Carousel items={ items } />
+                <Carousel items={ items } visibleItems={ visibleItems } itemWidth={ itemWidth } />
             </section>
         </div>
     );
